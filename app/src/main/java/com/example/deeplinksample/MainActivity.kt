05207a733b04184317ae52f60278bff6659cc186
EngiBeerHub <engibeer0422@gmail.com>
2023-10-15 19:39:46 +0900

    ADD: intent filter for custom url scheme and receive query parameters

--- app/src/main/java/com/example/deeplinksample/MainActivity.kt
+++ app/src/main/java/com/example/deeplinksample/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.deeplinksample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        intent?.data?.let { uri ->
+            val parameterValue = uri.getQueryParameter("code")
+            val state = uri.getQueryParameter("state")
+            Log.d("MainActivity", parameterValue.toString())
+            Log.d("MainActivity", state.toString())
+            Toast.makeText(this, "code: $parameterValue", Toast.LENGTH_SHORT).show()
+        }
     }
 }
